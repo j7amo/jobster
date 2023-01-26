e8c9eb96d875273fa4ee6f9c2fb83207b510aa0d
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-26 15:51:25 +0300

    add rate limiter for register, login routes

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
 // serve the frontend
 app.use(
   express.static(
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const rateLimiter = require('express-rate-limit');
 const testUser = require('../middleware/test-user');
+
+// we want to limit the number of requests from the same IP to prevent
+// bad actors from spamming our API
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: {
+    msg: 'Too many requests from your IP.Please try again after 15 minutes',
+  },
+});
 
 const router = express.Router();
 const authenticateUser = require('../middleware/authentication');
 const { register, login, updateUser } = require('../controllers/auth');
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', apiLimiter, register);
+router.post('/login', apiLimiter, login);
 // we add "updateUser" resource handling here. Because this is an authenticated resource(obviously
 // user cannot update his profile BEFORE logging in), we need to register auth middleware
 // before controller:
