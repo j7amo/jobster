bf239e7b5cfdd64e3a806bf5b68afad22ee1f057
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-26 10:53:56 +0300

    add updateUser functionality

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -43,7 +43,48 @@ const login = async (req, res) => {
   });
 };
 
+const updateUser = async (req, res) => {
+  const {
+    body: {
+      email, lastName, location, name,
+    },
+    user: { userId },
+  } = req;
+
+  if (!email || !lastName || !location || !name) {
+    throw new BadRequestError('Please provide email, lastname, location, name');
+  }
+
+  // here we update user in a different manner:
+  // 1) we find the user by its ID
+  const user = await User.findOne({ _id: userId });
+  // 2) change fields of the found User document
+  user.email = email;
+  user.lastname = lastName;
+  user.location = location;
+  user.name = name;
+  // 3) Save this document by inserting a new document into the database IF document.isNew is true.
+  // Or send an "updateOne" operation only with the modifications to the database.
+  // It does not replace the whole document in our case because we are NOT CREATING NEW
+  // DOCUMENT here, but we get the existing document from the DB. Which means that "$isNew" FLAG
+  // is FALSE:
+  await user.save();
+  // 4) Create a new token (because token payload includes NAME of the user which we CHANGE here)
+  const token = user.createJWT();
+  // 5) send back all the needed data
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastname,
+      location: user.location,
+      name: user.name,
+      token,
+    },
+  });
+};
+
 module.exports = {
   register,
   login,
+  updateUser,
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
-const { register, login } = require('../controllers/auth');
+const authenticateUser = require('../middleware/authentication');
+const { register, login, updateUser } = require('../controllers/auth');
 
 router.post('/register', register);
 router.post('/login', login);
+// we add "updateUser" resource handling here. Because this is an authenticated resource(obviously
+// user cannot update his profile BEFORE logging in), we need to register auth middleware
+// before controller:
+router.patch('/updateUser', authenticateUser, updateUser);
 
 module.exports = router;
