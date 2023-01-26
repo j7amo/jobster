219e5355ad0eb995cadda46bf9a929b1fa6941ab
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-26 17:08:07 +0300

    set up showStats controller with the help of MongoDB aggregation pipelines

--- src/controllers/jobs.js
+++ src/controllers/jobs.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const moment = require('moment');
 const { StatusCodes } = require('http-status-codes');
 const Job = require('../models/Job');
 const { BadRequestError, NotFoundError } = require('../errors');
@@ -129,10 +132,142 @@ const deleteJob = async (req, res) => {
   res.status(StatusCodes.OK).send();
 };
 
+const showStats = async (req, res) => {
+  // in order to extract and transform data from DB we can use Aggregation.
+  // Aggregation operations process multiple documents and return computed results.
+  // We can use aggregation operations to:
+  // - Group values from multiple documents together.
+  // - Perform operations on the grouped data to return a single result.
+  // - Analyze data changes over time.
+
+  // To perform aggregation operations, you can use:
+  // - Aggregation pipelines, which are the preferred method for performing aggregations.
+  //
+  // Single purpose aggregation methods, which are simple but lack the capabilities
+  // of an aggregation pipeline.
+  //
+  // Aggregation Pipelines
+  // An aggregation pipeline consists of one or more stages that process documents.
+  // Each stage performs an operation on the input documents.
+  // For example, a stage can (1)filter documents, (2)group documents, and (3)calculate values.
+  // The documents that are output from a stage are passed to the next stage.
+  // An aggregation pipeline can return results for groups of documents.
+  // For example, return the total, average, maximum, and minimum values.
+  let stats = await Job.aggregate([
+    // 1) filter by user associated with the job
+    {
+      $match: {
+        createdBy: mongoose.Types.ObjectId(req.user.userId),
+      },
+    },
+    // 2) group by status
+    {
+      $group: {
+        _id: '$status',
+        // 3) calculate: each time a job of the same status is found - count increment happens
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+
+  // we cannot use the "stats" data right away, we must transform it (because client
+  // expects a specific structure of the data):
+  stats = stats.reduce((acc, curr) => {
+    const { _id: status, count } = curr;
+    // eslint-disable-next-line no-underscore-dangle
+    acc[status] = count;
+
+    // return data in the format suitable for client
+    return acc;
+  }, {}); // results in { declined: 49, interview: 24, pending: 27 }
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplications = await Job.aggregate([
+    // filter
+    {
+      $match: {
+        createdBy: mongoose.Types.ObjectId(req.user.userId),
+      },
+    },
+    // group
+    {
+      $group: {
+        _id: {
+          // here we group jobs by YEAR & MONTH
+          year: {
+            // we use "$year" operator to extract year from "$createdAt" property
+            $year: '$createdAt',
+          },
+          month: {
+            // we use "$month" operator to extract month from "$createdAt" property
+            $month: '$createdAt',
+          },
+        },
+        // everytime we add such a document to the group we use "$sum" operator to increment value
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+    // sort
+    {
+      $sort: {
+        '_id.year': -1,
+        '_id.month': -1,
+      },
+    },
+    // limit
+    {
+      $limit: 6,
+    },
+  ]);
+  // The result of aggregation is:
+  // [
+  //   { _id: { year: 2023, month: 1 }, count: 1 },
+  //   { _id: { year: 2022, month: 12 }, count: 6 },
+  //   { _id: { year: 2022, month: 11 }, count: 4 },
+  //   { _id: { year: 2022, month: 10 }, count: 5 },
+  //   { _id: { year: 2022, month: 9 }, count: 2 },
+  //   { _id: { year: 2022, month: 8 }, count: 4 }
+  // ]
+  // we cannot use the "monthlyApplications" data right away, we must transform it (because client
+  // expects a specific structure of the data):
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+
+      const date = moment()
+        // we need to subtract 1 because "moment" treats month differently
+        .month(month - 1)
+        .year(year)
+        .format('MMM Y');
+
+      // return data in the format suitable for client
+      return {
+        date,
+        count,
+      };
+    })
+    .reverse();
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
+};
+
 module.exports = {
   createJob,
   deleteJob,
   getAllJobs,
   updateJob,
   getJob,
+  showStats,
 };
